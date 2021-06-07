7d49e2bc398b10b10a3900db1df6132ddc688288
naomi0260 <84354219+naomi0260@users.noreply.github.com>
2021-06-07 16:40:12 -0400

    Update App.java

--- src/main/java/ex16/App.java
+++ src/main/java/ex16/App.java
@@ -3,17 +3,8 @@ package ex16;
 import java.util.Scanner;
 
 /*
-Example Output
-What is your age? 15
-You are not old enough to legally drive.
-Or
-
-What is your age? 35
-You are old enough to legally drive.
-Constraints
-Use a single output statement.
-Use a ternary operator to write this program. If your language doesn’t support a ternary operator,
-use a regular if/else statement, and still use a single output statement to display the message.
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Naomi Palm
  */
 
 
